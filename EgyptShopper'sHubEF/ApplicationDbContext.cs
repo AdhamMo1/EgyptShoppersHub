0ddf7152531d591ae5183c0b0742670fd114fd90
Adham Mohamed <github.com/AdhamMo1>
2024-03-24 09:59:42 +0200

    Add project files.

---
+++ EgyptShopper'sHubEF/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EgyptShoppersHubCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgyptShoppersHubEF
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        
+    }
+}
